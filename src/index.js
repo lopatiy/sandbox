3565aafb404a5b221d8bd057650f23ae48e67f68
artem <artem.lopatiy.freshcode@gmail.com>
2017-08-31 11:24:14 +0300

    react router redux removed

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import {Provider} from 'react-redux';
 import App from './components/App';
 import store from './store';
 import {BrowserRouter as Router} from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory'
-import {syncHistoryWithStore} from 'react-router-redux';
-
 import registerServiceWorker from './registerServiceWorker';
-const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <App/>
-        </Router>
+        <Router><App/></Router>
     </Provider>,
     document.getElementById('root'));
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import articles from './articles';
 import {combineReducers} from 'redux';
-import {routerReducer} from 'react-router-redux';
 
 export default combineReducers({
-    routing: routerReducer,
     articles
 });
