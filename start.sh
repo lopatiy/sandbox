117d24a6893cfdc50d82eb343c1afed05df28cea
artem <artem.lopatiy.freshcode@gmail.com>
2017-09-11 17:24:05 +0300

    start sh improvement

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+npm i
+npm run build
+rm -r ./build/dropbox
+ln -s ~/Dropbox/ ./build/dropbox
+
 forever start -o ./prod.log -e ./prod.err.log ./src/prod.js
 forever start -o ./api.log -e ./api.err.log ./api/server.js
