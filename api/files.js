79f693b6a802272fe0db38dd2bed0d51c49c4fe2
lopatiy <noizejke@gmail.com>
2017-09-14 20:07:37 +0300

    cut command improved

--- api/files.js
+++ api/files.js
@@ -63,7 +63,7 @@ class FilesFS {
     }
 
     cutConstructor(filename, newFilename, start, end){
-        const command = `ffmpeg -ss ${start}.0 -i ${this.source}/${filename}.mp4 -c copy -t ${end}.0 ${this.source}/${newFilename}.mp4`;
+        const command = `ffmpeg -ss ${start}.0 -i ${this.source}/${filename}.mp4 -c copy -to ${end}.0 ${this.source}/${newFilename}.mp4`;
         console.log(`VIDEO CUT :: ${command}`);
         return command;
     }
